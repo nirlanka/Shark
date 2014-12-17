0039cb85587a95e54136b615f54f3b5d045070f4
Nirmal Lankathilaka <theshayn+nir@gmail.com>
2014-12-18 02:09:17 +0530

    Rename to testPacketsArrayList() in Extractor_test

--- src/Extractor_test.java
+++ src/Extractor_test.java
@@ -7,7 +7,7 @@ public class Extractor_test {
     // !! Extractor doesn't have methods :(
 
     @Test
-    public void testPackets() throws Exception {
+    public void testPacketsArrayList() throws Exception {
         // capturePackets() of Live_test
         Live_test live_test=new Live_test();
         live_test.capturePackets();
