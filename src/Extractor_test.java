5999e6d4434e5cb6ecd8207628c523974a09834b
Nirmal Lankathilaka <theshayn+nir@gmail.com>
2014-12-18 01:56:37 +0530

    Finish Extractor_test

--- src/Extractor_test.java
+++ src/Extractor_test.java
@@ -4,8 +4,14 @@ import static org.junit.Assert.*;
 
 public class Extractor_test {
 
-    @Test
-    public void t(){
+    // !! Extractor doesn't have methods :(
 
+    @Test
+    public void testPackets() throws Exception {
+        // capturePackets() of Live_test
+        Live_test live_test=new Live_test();
+        live_test.capturePackets();
+
+        assertEquals(0,Sea.packets.size());
     }
 }
